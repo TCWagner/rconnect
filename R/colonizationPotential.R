478f98a0493460aa9ce1efcdde352183d302ba80
Thomas C Wagner <theseus@mytum.de>
2022-08-24 19:56:52 +0200

    Berechnet nun das colonizationPotential als mean aller eC der Patches

--- R/colonizationPotential.R
+++ R/colonizationPotential.R
@@ -2,6 +2,7 @@
 #'
 #' @param habitats Raster containing patches of suitable habitats, coded with values > 0.
 #' @param kernel Dispersal kernel of the species.
+#' @param threshold Minimum of eC at which two patches are connected.
 #' @param cap If true, values larger than 1 are capped.
 #' @param summarize If TRUE, a table instead of a raster is returned.
 #'
@@ -12,23 +13,17 @@
 #' example <- system.file("extdata/cc_clumped_lech_ehd.tif", package = "rconnect")
 #' habitats_lech <- raster::raster(example)
 #' sddkernel_chondrilla <- dispersalKernel(cellsize=5, radius=3, decay=0.19)
-#' cP <- colonizationPotential(habitats_lech, sddkernel_chondrilla, cap=TRUE)
+#' cP <- colonizationPotential(habitats_lech, sddkernel_chondrilla, threshold=0.01, cap=TRUE)
 #'
 
-colonizationPotential <- function(habitats, kernel, cap=FALSE, summarize=TRUE){
-  res <- effectiveSeedrain(habitats, kernel, summarize=summarize, fun=sum)
-  if(summarize==T){
-    names(res) <- c("patch","cP")
-    if(cap==T){
-      res$cP[res$cP[]>1] <- 1 # make sure each habitat counts max as 1
-    }
-    return(res)
-  }
-  z <- raster::clump(habitats)
-  raster::zonal(res, z, fun=sum)
+colonizationPotential <- function(habitats, kernel, threshold=0, cap=FALSE, summarize=TRUE){
+  res <- effectiveConnections(habitats, kernel, threshold)
+
+  cp <- mean(res$eC, na.rm=T)
   if(cap){
-    res <- res*(res<=1)+1*(res>1)
+    cp <- min(cp,1)
   }
-  return(res)
-# test
+
+  return(cp)
 }
+

daea76672f73d1e45793e6538e49a065268d2e24
Thomas C Wagner <theseus@mytum.de>
2022-08-24 19:51:50 +0200

    Neue Funktion für connectionCapacity

---
+++ R/connectionCapacity.R
@@ -0,0 +1,41 @@
+#' Determine the connection Capacity (cC) for connected patches
+#'
+#' @param habitats Raster containing patches of suitable habitats, coded with values > 0.
+#' @param kernel Dispersal kernel of the species.
+#' @param threshold Minimum fraction of eC at which two patches are connected.
+#' @param summarize If TRUE, a table instead of a raster is returned.
+#'
+#' @return A table or raster with the connection Capacity (cC) for each Patch
+#' @export
+#'
+#' @examples
+#' example <- system.file("extdata/cc_clumped_lech_ehd.tif", package = "rconnect")
+#' habitats_lech <- raster::raster(example)
+#' sddkernel_chondrilla <- dispersalKernel(cellsize=5, radius=3, decay=0.19)
+#' nC <- connectionCapacity(habitats_lech, sddkernel_chondrilla, threshold=0.01)
+#'
+#'
+connectionCapacity <- function(habitats, kernel, threshold=0.05, summarize=TRUE){
+
+  ecm <- effectiveConnectionsMatrix(habitats, kernel, threshold)
+  ec <- colSums(ecm, na.rm=T)
+  nc <- colSums(ecm>threshold, na.rm=T)
+  zs <- data.frame(patch=seq(1,length(ec)), cC=ec/nc)
+
+  zs$cC[is.nan(zs$cC)] <- 0
+
+
+  if(summarize){
+    return(zs)
+  }
+
+  # else reclassify and return raster
+
+  rc <- raster::clump(habitats>=0)
+  rc2 <- raster::reclassify(rc, zs)
+  result <- rc2
+  names(result) <- c("nC")
+
+  return(result)
+
+}
